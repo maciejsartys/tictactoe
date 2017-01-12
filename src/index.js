2c0e0c0bfc7da8530518e1ef943d6cae00ca6cdc
Maciej Sartys <sartys.maciej@gmail.com>
2017-01-12 21:48:19 +0000

    webpack updated to v2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require('./css/main.scss')
-import Ui from './js/ui'
+import Game from './js/game'
 
-const ui = new Ui()
-ui.showMark('r0c0', 'x')
+const game = new Game()
+game.start()
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,27 @@
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
-  module: {
-    loaders: [{
-      enforce: 'pre',
-      test: /\.js$/,
-      loader: 'eslint-loader',
-      exclude: /node_modules/
-      }, {
-      test: /\.js?$/,
-      exclude: /node_modules/,
-      loader: 'babel'
-    }, {
-      test: /\.scss$/,
-      loaders: ['style', 'css', 'sass']
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js']
-  },
+  entry: './src/index.js',
+  devtool: 'source-map',
   output: {
     path: __dirname + '/static/js',
     publicPath: '/',
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      enforce: 'pre',
+      test: /\.js$/,
+      loader: 'eslint-loader',
+      exclude: [/node_modules/, /static/]
+      }, {
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }, {
+      test: /\.scss$/,
+      loaders: ['style-loader', 'css-loader', 'sass-loader']
+    }]
+  },
+  resolve: {
+    extensions: ['.js']
   },
 };
